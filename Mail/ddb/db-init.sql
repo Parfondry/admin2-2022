8362c4590ff4d80e26360a820b962ef26af7131e
CastermaneRobin <robin@castermane.be>
2022-05-18 11:26:03 +0100

    Ajout du script pour la base de données mail (création des tables + ajout des utilisateurs/domaines/alias)

---
+++ Mail/ddb/db-init.sql
@@ -0,0 +1,32 @@
+CREATE TABLE `virtual_domains` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `virtual_users` (
+  `id` int(11) NOT NULL auto_increment,
+  `domain_id` int(11) NOT NULL,
+  `password` varchar(106) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `virtual_aliases` (
+  `id` int(11) NOT NULL auto_increment,
+  `domain_id` int(11) NOT NULL,
+  `source` varchar(100) NOT NULL,
+  `destination` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO mailserver.virtual_domains (name) VALUES ('m1-1.ephec-ti.be');
+
+INSERT INTO mailserver.virtual_users (domain_id, password , email) VALUES ('1', '***', 'directeur@m1-1.ephec-ti.be'), ('1', '***', 'secretariat@m1-1.ephec-ti.be'), ('1', '***', 'robin@m1-1.ephec-ti.be');
+INSERT INTO mailserver.virtual_users (domain_id, password , email) VALUES ('1', '***', 'atelier@m1-1.ephec-ti.be');
+
+INSERT INTO mailserver.virtual_aliases (domain_id, source, destination) VALUES ('1', 'directeur@m1-1.ephec-ti.be', 'robin@castermane.be');
