5a29c8488ed73124f588442cbe1a7a5cc6d93fd2
root <root@m1-1.ephec-ti.be>
2022-06-08 09:13:53 +0100

    ajout de fichier pour automatiser le déploiement du service mail (.sh)

---
+++ Mail/Postfix/download.sh
@@ -0,0 +1,6 @@
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/mail/Mail/Postfix/main.cf
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/mail/Mail/Postfix/master.cf
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/mail/Mail/Postfix/mysql-virtual-alias-maps.cf
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/mail/Mail/Postfix/mysql-virtual-email2email.cf
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/mail/Mail/Postfix/mysql-virtual-mailbox-domains.cf
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/mail/Mail/Postfix/mysql-virtual-mailbox-maps.cf
