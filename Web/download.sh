5c245f70fbfefccc195cb7ee3e6265734c94676b
NathanGregoar <he201832@students.ephec.be>
2022-06-08 18:30:00 +0200

    add downloadsh

---
+++ Web/download.sh
@@ -0,0 +1,7 @@
+#Web
+mkdir web && cd web
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/web/Web/nginx.conf
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/web/Web/docker-compose.yml
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/web/Web/Dockerfile
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/web/Web/vitrine/index.html
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/web/Web/b2b/index.php
