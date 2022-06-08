69a3b481f5eb105be9556510e2517b3c80958def
Parfondry <64077936+Parfondry@users.noreply.github.com>
2022-06-08 18:16:28 +0200

    Update download.sh

--- Services Internes/download.sh
+++ Services Internes/download.sh
@@ -3,16 +3,16 @@
 curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/main/Services Internes/docker-compose.yml
 mkdir soa && cd resolver
 curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/main/Services Internes/SOA interne/Dockerfile
-curl -o https://github.com/Parfondry/admin2-2022/blob/main/Services Internes/SOA interne/named.conf
-curl -o https://github.com/Parfondry/admin2-2022/blob/main/Services Internes/SOA interne/named.conf.local
-curl -o https://github.com/Parfondry/admin2-2022/blob/main/Services Internes/SOA interne/named.conf.options
-curl -o https://github.com/Parfondry/admin2-2022/blob/main/Services Internes/SOA interne/named.conf.default-zones
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/blob/main/Services Internes/SOA interne/named.conf
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/blob/main/Services Internes/SOA interne/named.conf.local
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/blob/main/Services Internes/SOA interne/named.conf.options
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/blob/main/Services Internes/SOA interne/named.conf.default-zones
 curl -o https://github.com/Parfondry/admin2-2022/blob/main/Services Internes/SOA interne/db.intranet.m1-1.ephec-ti.be
 
 cd..
 mkdir resolver && cd resolver
-curl -o https://github.com/Parfondry/admin2-2022/blob/main/Services Internes/resolver/Dockerfile
-curl -o https://github.com/Parfondry/admin2-2022/blob/main/Services Internes/resolver/named.conf
-curl -o https://github.com/Parfondry/admin2-2022/blob/main/Services Internes/resolver/named.conf.local
-curl -o https://github.com/Parfondry/admin2-2022/blob/main/Services Internes/resolver/named.conf.options
-curl -o https://github.com/Parfondry/admin2-2022/blob/main/Services Internes/resolver/named.conf.default-zones
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/blob/main/Services Internes/resolver/Dockerfile
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/blob/main/Services Internes/resolver/named.conf
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/blob/main/Services Internes/resolver/named.conf.local
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/blob/main/Services Internes/resolver/named.conf.options
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/blob/main/Services Internes/resolver/named.conf.default-zones
