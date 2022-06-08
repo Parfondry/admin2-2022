c884a3915e7f2aa9d9381c32cc0c2ce45e95ec8d
NathanGregoar <he201832@students.ephec.be>
2022-06-08 18:17:17 +0200

    Add downloadsh

---
+++ VoIP/download.sh
@@ -0,0 +1,8 @@
+
+#VoIP
+mkdir voip && cd voip
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/voip/VoIP/Dockerfile
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/voip/VoIP/config/users.conf
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/voip/VoIP/config/extensions.conf
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/voip/VoIP/config/sip.conf
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/voip/VoIP/config/voicemail.conf
