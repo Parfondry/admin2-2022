eed4221999313eb61f8cd75353afcfe2fe2b61f7
Parfondry <64077936+Parfondry@users.noreply.github.com>
2022-06-08 19:28:53 +0200

    Update download.sh

--- DNS/test/download.sh
+++ DNS/test/download.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
 
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/main/DNS/test/docker-compose.yml
+mkdir externe 
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/main/DNS/test/bind/Dockerfile
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/main/DNS/test/bind/named.conf
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/main/DNS/test/bind/named.conf.local
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/main/DNS/test/bind/named.conf.options
+curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/main/DNS/test/bind/named.conf.default-zones
+curl -o https://github.com/Parfondry/admin2-2022/main/DNS/test/bind/db.intranet.m1-1.ephec-ti.be
