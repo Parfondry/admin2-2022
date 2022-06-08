145b5471dc6770d5325ee9c523c517ad4246811f
Parfondry <64077936+Parfondry@users.noreply.github.com>
2022-06-08 19:33:51 +0200

    Update download.sh

--- DNS/test/download.sh
+++ DNS/test/download.sh
@@ -6,5 +6,4 @@ curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/main/DNS/test/bi
 curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/main/DNS/test/bind/named.conf
 curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/main/DNS/test/bind/named.conf.local
 curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/main/DNS/test/bind/named.conf.options
-curl -o https://raw.githubusercontent.com/Parfondry/admin2-2022/main/DNS/test/bind/named.conf.default-zones
-curl -o https://github.com/Parfondry/admin2-2022/main/DNS/test/bind/db.intranet.m1-1.ephec-ti.be
+curl -o https://github.com/Parfondry/admin2-2022/main/DNS/test/bind/db.m1-1.ephec-ti.be
