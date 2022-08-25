11b5944423472eed434cadbe3bcbc6b4d1b9c22f
Parfondry <64077936+Parfondry@users.noreply.github.com>
2022-08-25 02:12:20 +0200

    Delete download.sh

--- Services Internes/user/download.sh
+++
@@ -1,2 +0,0 @@
-curl -o docker-compose.yml https://raw.githubusercontent.com/Parfondry/admin2-2022/main/Services Internes/docker-compose.yml
-curl -o resolv.conf https://raw.githubusercontent.com/Parfondry/admin2-2022/main/Services Internes/resolv.conf
